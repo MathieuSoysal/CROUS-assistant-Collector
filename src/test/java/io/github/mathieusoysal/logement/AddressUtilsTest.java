2c7134bda4ae3a66d4d805fc3c682e76faf72506
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2023-10-26 22:51:35 +0000

    🧪 Add failing test

--- src/test/java/io/github/mathieusoysal/logement/AddressUtilsTest.java
+++ src/test/java/io/github/mathieusoysal/logement/AddressUtilsTest.java
@@ -31,4 +31,16 @@ class AddressUtilsTest {
         String street = AddressUtils.getStreetFromString(address);
         assertEquals(expectedStreet, street);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "123 Main St 84       city de la garde;84",
+            "123 -  - - 8451 8451 Main St     84450 city;84450",
+            "123 Main St     84450       11841 city;11841",
+            "456 Main St 8484 84450 city;84450"
+    }, delimiter = ';')
+    void testGetZipCodeFromString(String address, String expectedZipCode) {
+        String zipCode = AddressUtils.getZipCodeFromString(address);
+        assertEquals(expectedZipCode, zipCode);
+    }
 }

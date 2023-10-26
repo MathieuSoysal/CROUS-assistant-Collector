7805fcd551cfc26d1445dfacc7cfb1159237f850
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2023-10-26 22:42:32 +0000

    ✅ Validate test

--- src/main/java/io/github/mathieusoysal/logement/AddressUtils.java
+++ src/main/java/io/github/mathieusoysal/logement/AddressUtils.java
@@ -1,9 +1,10 @@
 package io.github.mathieusoysal.logement;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class AddressUtils {
-    private static final Pattern PATTERN_REGEX_CITY = Pattern.compile("(\\d =?)[^\\d]*$", Pattern.MULTILINE);
+    private static final Pattern PATTERN_REGEX_CITY = Pattern.compile("([^ \\d]+[^\\d]*$)", Pattern.MULTILINE);
     private static final Pattern PATTER_REGEX_ZIP_CODE = Pattern.compile("\\d*(?=[a-zA-Z]*[^\\d]*$)",
             Pattern.MULTILINE);
     private static final Pattern PATTERN_REGEX_STREET = Pattern.compile(".*[^ ](?= +[a-zA-Z]*[\\d]+ +[^\\d]*$)",
@@ -13,15 +14,18 @@ class AddressUtils {
     }
 
     public static String getCityFromString(String address) {
-        return PATTERN_REGEX_CITY.matcher(address).group(1);
-    }
-
-    public static String getZipCodeFromString(String address) {
-        return PATTER_REGEX_ZIP_CODE.matcher(address).group(1);
+        Matcher matcher = PATTERN_REGEX_CITY.matcher(address);
+        if (matcher.find())
+            return matcher.group(0);
+        return "";
     }
 
     public static String getStreetFromString(String address) {
-        return PATTERN_REGEX_STREET.matcher(address).group(1);
+        return null;
+    }
+
+    public static String getZipCodeFromString(String address) {
+        return null;
     }
 
 }

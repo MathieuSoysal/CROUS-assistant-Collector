b4b68ea32c40877c50647a08e5435ae459f53e2f
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2023-10-27 12:47:45 +0000

    🧪 Add failing test BedKind

--- src/main/java/io/github/mathieusoysal/logement/BedKind.java
+++ src/main/java/io/github/mathieusoysal/logement/BedKind.java
@@ -11,18 +11,21 @@ public enum BedKind {
     DOUBLE,
     MOBILE,
     RAISED,
+    NONE,
     UNKNOWN;
 
     /**
      * Returns the BedKind corresponding to the given string.
      * 
      * @param bedKind the string representation of the BedKind
-     * @return the corresponding BedKind, or UNKNOWN if the string is null, blank or "null"
-     * @throws IllegalArgumentException if the string does not correspond to any BedKind
+     * @return the corresponding BedKind, or NONE if the string is null, blank or
+     *         "null"
+     * @throws IllegalArgumentException if the string does not correspond to any
+     *                                  BedKind
      */
     public static BedKind fromString(String bedKind) {
-        if (bedKind == null || bedKind.isBlank() || bedKind.equals("null")) 
-            return UNKNOWN;
+        if (bedKind == null || bedKind.isBlank() || bedKind.equals("null"))
+            return NONE;
         switch (bedKind) {
             case "simple":
                 return SIMPLE;

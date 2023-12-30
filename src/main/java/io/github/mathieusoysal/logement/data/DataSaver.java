fd397176dec87aa65b17a2fc03c828aa376ea78d
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2023-12-30 14:04:55 +0000

    Add DataSaver and DataSaverTest classes

---
+++ src/main/java/io/github/mathieusoysal/logement/data/DataSaver.java
@@ -0,0 +1,62 @@
+package io.github.mathieusoysal.logement.data;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.DateTimeException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import io.github.mathieusoysal.logement.pojo.Logement;
+
+public class DataSaver {
+
+    public static File createArchiveFolder() {
+        File archiveFolder = new File("archive");
+        if (!archiveFolder.exists())
+            archiveFolder.mkdir();
+        return archiveFolder;
+    }
+
+    public static File createArchiveLogements(List<Logement> logements) throws JsonProcessingException {
+        File archiveFolder = getArchiveFolderForCurrentDate();
+        String logementsJson = convertLogementsToJson(logements);
+        File archiveFile = getArchiveFile(archiveFolder);
+        writeLogementsDataInsideArchiveFile(logementsJson, archiveFile);
+        return archiveFile;
+    }
+
+    private static void writeLogementsDataInsideArchiveFile(String logementsJson, File archiveFile) {
+        try (FileWriter fileWriter = new FileWriter(archiveFile)) {
+            fileWriter.write(logementsJson);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while writing logements to file", e);
+        }
+    }
+
+    private static File getArchiveFile(File archiveFolder) throws DateTimeException {
+        String archiveFileName = OffsetDateTime.now().toLocalTime().format(DateTimeFormatter.ofPattern("HH"));
+        File archiveFile = new File(archiveFolder, archiveFileName);
+        return archiveFile;
+    }
+
+    private static String convertLogementsToJson(List<Logement> logements) throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        return ow.writeValueAsString(logements);
+    }
+
+    private static File getArchiveFolderForCurrentDate() {
+        File archiveFolder = createArchiveFolder();
+        String archiveFolderName = OffsetDateTime.now().toLocalDate().toString();
+        File archiveFile = new File(archiveFolder, archiveFolderName);
+        if (!archiveFile.exists())
+            archiveFile.mkdir();
+        return archiveFile;
+    }
+
+}

4d4e6cc53379873e2abd44921b1d077259d94702
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2024-01-27 15:33:49 +0000

    ✅ Fix regex pattern for street matching
    fix FIX Address bug #46

--- src/main/java/io/github/mathieusoysal/logement/AddressUtils.java
+++ src/main/java/io/github/mathieusoysal/logement/AddressUtils.java
@@ -7,7 +7,7 @@ class AddressUtils {
     private static final Pattern PATTERN_REGEX_CITY = Pattern.compile("[^ \\d]+[^\\d]*[\\d ]*$", Pattern.MULTILINE);
     private static final Pattern PATTER_REGEX_ZIP_CODE = Pattern.compile("\\d*(?=[a-zA-Z]*[^\\d]*$)",
             Pattern.MULTILINE);
-    private static final Pattern PATTERN_REGEX_STREET = Pattern.compile(".*[^ ](?= +[a-zA-Z]*-?[\\d]+ +[^\\d]*$)",
+    private static final Pattern PATTERN_REGEX_STREET = Pattern.compile("^.*[^ \\-](?= +[a-zA-Z]*[- ]*[\\d]{4,} +[^\\d]*[\\d ]*$)",
             Pattern.MULTILINE);
 
     private AddressUtils() {

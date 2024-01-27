f9a78e133f3619e97a1fb61f6cdecab34f66dd58
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2024-01-27 14:40:27 +0000

    Fix regex pattern for city and zip code matching
    fix FIX Address bug #46

--- src/main/java/io/github/mathieusoysal/logement/AddressUtils.java
+++ src/main/java/io/github/mathieusoysal/logement/AddressUtils.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class AddressUtils {
-    private static final Pattern PATTERN_REGEX_CITY = Pattern.compile("[^ \\d]+[^\\d]*$", Pattern.MULTILINE);
+    private static final Pattern PATTERN_REGEX_CITY = Pattern.compile("[^ \\d]+[^\\d]*[\\d ]*$", Pattern.MULTILINE);
     private static final Pattern PATTER_REGEX_ZIP_CODE = Pattern.compile("\\d*(?=[a-zA-Z]*[^\\d]*$)",
             Pattern.MULTILINE);
     private static final Pattern PATTERN_REGEX_STREET = Pattern.compile(".*[^ ](?= +[a-zA-Z]*-?[\\d]+ +[^\\d]*$)",

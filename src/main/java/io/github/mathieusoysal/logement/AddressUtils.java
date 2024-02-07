0f9b588858f4b1674c97103fe2eb034cd5509266
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2024-02-07 18:59:31 +0000

    Validation zip code fix #62

--- src/main/java/io/github/mathieusoysal/logement/AddressUtils.java
+++ src/main/java/io/github/mathieusoysal/logement/AddressUtils.java
@@ -5,9 +5,11 @@ import java.util.regex.Pattern;
 
 class AddressUtils {
     private static final Pattern PATTERN_REGEX_CITY = Pattern.compile("[^ \\d]+[^\\d]*[\\d ]*$", Pattern.MULTILINE);
-    private static final Pattern PATTER_REGEX_ZIP_CODE = Pattern.compile("\\d+(?=[a-zA-Z]*[^\\d]*[\\d ]*$)",
+    private static final Pattern PATTER_REGEX_ZIP_CODE = Pattern.compile(
+            "( \\d{2} )?[\\d]+(?=[a-zA-Z]*[^\\d]*[\\d\\. ]*$)",
             Pattern.MULTILINE);
-    private static final Pattern PATTERN_REGEX_STREET = Pattern.compile("^.*[^ \\-](?= +[a-zA-Z]*[- ]*[\\d]{4,} +[^\\d]*[\\d ]*$)",
+    private static final Pattern PATTERN_REGEX_STREET = Pattern.compile(
+            "^.*[^ \\-](?= +[a-zA-Z]*[- ]*[\\d]{4,} +[^\\d]*[\\d ]*$)",
             Pattern.MULTILINE);
 
     private AddressUtils() {
@@ -30,7 +32,7 @@ class AddressUtils {
     static String getZipCodeFromString(String address) {
         Matcher matcher = PATTER_REGEX_ZIP_CODE.matcher(address);
         if (matcher.find())
-            return matcher.group(0);
+            return matcher.group(0).replaceAll(" ", "");
         return "";
     }
 

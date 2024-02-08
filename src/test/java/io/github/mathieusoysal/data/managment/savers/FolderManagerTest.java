8ab6ab359574e69c03811add7d2b3fc8d87e4a47
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2024-02-08 23:03:33 +0000

    Add tearDown method to delete archive folder after each test

--- src/test/java/io/github/mathieusoysal/data/managment/savers/FolderManagerTest.java
+++ src/test/java/io/github/mathieusoysal/data/managment/savers/FolderManagerTest.java
@@ -6,9 +6,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.time.OffsetDateTime;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 class FolderManagerTest {
+
+    @AfterEach
+    void tearDown() {
+        File archiveFolder = new File("archive");
+        if (archiveFolder.exists())
+            archiveFolder.delete();
+    }
 
     @Test
     void testCreateArchiveFolder() {
@@ -17,7 +25,6 @@ class FolderManagerTest {
         assertTrue(archiveFolder.exists());
         assertTrue(archiveFolder.isDirectory());
         assertEquals("archive", archiveFolder.getName());
-        archiveFolder.delete();
     }
 
     @Test

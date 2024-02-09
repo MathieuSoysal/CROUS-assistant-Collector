65210c4cde80076da374b740844582df7919b9a3
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2024-02-09 15:44:15 +0000

    Add assertions for file existence in ArchiveHourTest

--- src/test/java/io/github/mathieusoysal/archivers/ArchiveHourTest.java
+++ src/test/java/io/github/mathieusoysal/archivers/ArchiveHourTest.java
@@ -1,6 +1,10 @@
 package io.github.mathieusoysal.archivers;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
@@ -16,6 +20,10 @@ public class ArchiveHourTest {
         System.setProperty(Properties.PASSWORD.getName(), System.getenv("TEST_PASSWORD"));
 
         assertDoesNotThrow(() -> Archivers.ARCHIVER_HOUR.archive());
+        assertTrue(Files.exists(Path.of("archive")));
+        assertTrue(Files.exists(Path.of("archive/residences")));
+        assertTrue(Files.exists(Path.of("archive/hash_residences")));
+        assertTrue(Files.exists(Path.of("archive/available-residences-id")));
     }
 
 }

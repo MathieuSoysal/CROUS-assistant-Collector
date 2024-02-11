062f889e3668e9ee90548a19309a090964d0a327
Mathieu Soysal <43273304+MathieuSoysal@users.noreply.github.com>
2024-02-11 18:23:03 +0000

    Add failed test for day archive
    fix #64

--- src/test/java/io/github/mathieusoysal/data/managment/savers/DataSaverTest.java
+++ src/test/java/io/github/mathieusoysal/data/managment/savers/DataSaverTest.java
@@ -1,13 +1,17 @@
 package io.github.mathieusoysal.data.managment.savers;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.github.mathieusoysal.data.managment.collectors.DataCollectorFromArchive;
@@ -16,6 +20,23 @@ import io.github.mathieusoysal.exceptions.requests.ApiRequestFailedException;
 import io.github.mathieusoysal.residence.Residence;
 
 class DataSaverTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     @AfterEach
     void tearDown() {
@@ -26,7 +47,8 @@ class DataSaverTest {
 
     @Test
     void testCreateArchiveResidences() throws ApiRequestFailedException, IOException {
-        List<Residence> residences = DataCollectorFromCrous.getAllResidencesWithoutConnection().stream().limit(2).toList();
+        List<Residence> residences = DataCollectorFromCrous.getAllResidencesWithoutConnection().stream().limit(2)
+                .toList();
         assertDoesNotThrow(() -> ArchiveSaver.startPath().endPathAndSaveData(ArchiveName.HOUR, residences));
     }
 
@@ -34,16 +56,15 @@ class DataSaverTest {
     void testCreateArchiveResidencesForDay() throws ApiRequestFailedException, IOException {
         LocalDate chosenDate = LocalDate.of(2024, 1, 10);
         var dataCollector = new DataCollectorFromArchive(
-                "https://mathieusoysal.github.io/CROUS-assistant-Collector/v2/residences-crous/available/");
-        String data = assertDoesNotThrow(() -> 
-            dataCollector.getSumUpOfDay(chosenDate)
-        );
+                "https://mathieusoysal.github.io/CROUS-assistant-Collector/v2");
+        String data = assertDoesNotThrow(() -> dataCollector.getSumUpOfDay(chosenDate));
         assertDoesNotThrow(
                 () -> ArchiveSaver
                         .startPath()
                         .addPath("available")
                         .addPath(chosenDate)
                         .endPathAndSaveData(ArchiveName.DAY_SUM_UP, data));
+        assertNotEquals("[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]", data);
     }
 
 }

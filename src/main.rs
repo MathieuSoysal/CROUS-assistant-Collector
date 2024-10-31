a8c8607b924f56ac3e25cca975af64cff8da4f91
Mathieu Soysal <Mathieu.soysal@outlook.fr>
2024-10-31 12:21:10 +0000

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,19 @@
-use log::{debug, error, info, warn};
-use mongodb::{
-    bson::{doc, Bson, Document},
-    options::{ClientOptions, ServerApi, ServerApiVersion},
-    Client, Collection,
-};
-use serde_json::Value;
+use log::{debug, error, info};
+
 use std::env;
 use std::error::Error;
+pub mod crous_mongodb;
+pub mod requestor;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
     info!("Starting the MongoDB JSON Importer.");
     let mongodb_uri = get_mongodb_uri()?;
-    let url = "https://trouverunlogement.lescrous.fr/api/fr/search/36";
-    let json_request_body = build_request_body();
-    let response_json = execute_http_request(url, &json_request_body).await?;
-    let items_node = extract_items_node(&response_json)?;
-    let client = connect_to_mongodb(&mongodb_uri).await?;
-    let ids = insert_logements_into_mongodb(
-        &client.database("CROUS").collection::<Document>("logements"),
-        items_node,
-    )
-    .await?;
-    insert_ids_with_timestamp_into_mongodb(
-        &client.database("CROUS").collection::<Document>("available"),
-        ids,
-    )
-    .await?;
+    let logements = requestor::get_logements_from_crous().await?;
+    let client = crous_mongodb::connect_to_mongodb(&mongodb_uri).await?;
+    let ids = crous_mongodb::insert_logements_into_mongodb(&client, &logements).await?;
+    crous_mongodb::insert_ids_with_timestamp_into_mongodb(&client, ids).await?;
     info!("Data imported successfully.");
     Ok(())
 }
@@ -43,120 +29,4 @@ fn get_mongodb_uri() -> Result<String, Box<dyn Error>> {
             Err("MONGODB_URI environment variable not set".into())
         }
     }
-}
-
-fn build_request_body() -> serde_json::Value {
-    info!("Building JSON request body.");
-    serde_json::json!({
-        "idTool": 36,
-        "need_aggregation": true,
-        "page": 1,
-        "pageSize": 2500,
-        "sector": null,
-        "occupationModes": [],
-        "location": [
-            { "lon": -9.9079, "lat": 51.7087 },
-            { "lon": 14.3224, "lat": 40.5721 }
-        ],
-        "residence": null,
-        "precision": 3,
-        "equipment": [],
-        "adaptedPmr": false,
-        "toolMechanism": "residual"
-    })
-}
-
-async fn execute_http_request(
-    url: &str,
-    json_body: &serde_json::Value,
-) -> Result<serde_json::Value, Box<dyn Error>> {
-    info!("Executing HTTP POST request to {}", url);
-    let client = reqwest::Client::new();
-    let response = client.post(url).json(json_body).send().await?;
-    if response.status().is_success() {
-        debug!("HTTP response status: {}", response.status());
-        let response_json: serde_json::Value = response.json().await?;
-        Ok(response_json)
-    } else {
-        error!(
-            "HTTP request failed with status code: {}",
-            response.status()
-        );
-        Err(format!(
-            "HTTP request failed with status code: {}",
-            response.status()
-        )
-        .into())
-    }
-}
-
-fn extract_items_node(response_json: &serde_json::Value) -> Result<&Value, Box<dyn Error>> {
-    info!("Extracting items.");
-    let items_node = response_json
-        .pointer("/results/items")
-        .ok_or("Items node not found in the response")?;
-    debug!("Extracted items node.");
-    Ok(items_node)
-}
-
-async fn connect_to_mongodb(mongodb_uri: &str) -> Result<Client, Box<dyn Error>> {
-    info!("Connecting to MongoDB at {}", mongodb_uri);
-    let mut client_options = ClientOptions::parse(mongodb_uri).await?;
-    client_options.app_name = Some("CROUS analytics".to_string());
-    let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
-    client_options.server_api = Some(server_api);
-    let client = Client::with_options(client_options)?;
-    client
-        .database("CROUS")
-        .run_command(doc! { "ping": 1 })
-        .await
-        .map_err(|e| {
-            error!("Failed to connect to MongoDB: {}", e);
-            e
-        })?;
-    info!("Successfully connected to MongoDB.");
-    Ok(client)
-}
-
-async fn insert_logements_into_mongodb(
-    collection: &Collection<Document>,
-    items_node: &serde_json::Value,
-) -> Result<Vec<Bson>, Box<dyn Error>> {
-    let mut ids = Vec::new();
-    if let Some(items_array) = items_node.as_array() {
-        info!("Inserting {} items into MongoDB.", items_array.len());
-        for item_value in items_array {
-            let bson_value = mongodb::bson::to_bson(&item_value)?;
-            if let Bson::Document(mut doc) = bson_value {
-                if let Some(id_value) = doc.get("id").cloned() {
-                    doc.insert("_id", id_value.clone());
-                    ids.push(id_value.clone());
-                    let filter = doc! { "_id": id_value.clone() };
-                    collection.replace_one(filter, doc).await?;
-                    debug!("Inserted/Updated document with _id: {:?}", id_value);
-                } else {
-                    warn!("Item missing 'id' field: {:?}", doc);
-                }
-            } else {
-                warn!("Expected a document but got a different BSON type.");
-            }
-        }
-        Ok(ids)
-    } else {
-        error!("Items node is not an array.");
-        Err("Items node is not an array".into())
-    }
-}
-
-async fn insert_ids_with_timestamp_into_mongodb(
-    collection: &Collection<Document>,
-    ids: Vec<Bson>,
-) -> Result<(), Box<dyn Error>> {
-    let doc = doc! {
-        "timestamp": mongodb::bson::DateTime::now(),
-        "ids": ids,
-    };
-    collection.insert_one(doc).await?;
-    info!("Inserted ids into timestamp collection.");
-    Ok(())
 }
---
+++ src/requestor.rs
@@ -0,0 +1,60 @@
+use log::{debug, error, info};
+
+use serde_json::Value;
+use std::error::Error;
+
+const URL_CROUS: &str = "https://trouverunlogement.lescrous.fr/api/fr/search/36";
+
+pub async fn get_logements_from_crous() -> Result<serde_json::Value, Box<dyn Error>> {
+    let response_json = request_to_crous().await?;
+    let items_node = extract_items_node(&response_json)?;
+    Ok(items_node.clone())
+}
+
+async fn build_request_body() -> serde_json::Value {
+    info!("Building JSON request body.");
+    serde_json::json!({
+        "idTool": 36,
+        "need_aggregation": true,
+        "page": 1,
+        "pageSize": 2500,
+        "sector": null,
+        "occupationModes": [],
+        "location": [
+            { "lon": -9.9079, "lat": 51.7087 },
+            { "lon": 14.3224, "lat": 40.5721 }
+        ],
+        "residence": null,
+        "precision": 3,
+        "equipment": [],
+        "adaptedPmr": false,
+        "toolMechanism": "residual"
+    })
+}
+
+async fn request_to_crous() -> Result<serde_json::Value, Box<dyn Error>> {
+    let json_body = build_request_body().await;
+    info!("Executing HTTP POST request to {}", URL_CROUS);
+    let client = reqwest::Client::new();
+    let response = client.post(URL_CROUS).json(&json_body).send().await?;
+    if response.status().is_success() {
+        debug!("HTTP response status: {}", response.status());
+        Ok(response.json().await?)
+    } else {
+        let message = format!(
+            "HTTP request failed with status code: {}",
+            response.status()
+        );
+        error!("{}", message);
+        Err(message.into())
+    }
+}
+
+fn extract_items_node(response_json: &serde_json::Value) -> Result<&Value, Box<dyn Error>> {
+    info!("Extracting items.");
+    let items_node = response_json
+        .pointer("/results/items")
+        .ok_or("Items node not found in the response")?;
+    debug!("Extracted items node.");
+    Ok(items_node)
+}
